98c8dcd6d4b410dc08f11c249bd6346bcacbd1ea
Limamat1as <limamatias2002@gmail.com>
2025-02-23 15:42:16 -0300

    update

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ def main():
 
         with col2:
             # Exibição da visualização (imagem da página)
-            with open("Contra_Cheque.pdf", "rb") as f:
+            with open(uploaded_file, "rb") as f:
                 base64_pdf = base64.b64encode(f.read()).decode("utf-8")
             pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}#zoom=100" width="100%" height="1000" type="application/pdf"></iframe>'
             st.markdown(pdf_display, unsafe_allow_html=True)
